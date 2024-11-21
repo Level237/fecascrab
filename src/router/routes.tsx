64ca7090a50667867ef406bfb35095574e8ce0ec
level237 <Bramslevel129@gmail.com>
2024-11-21 11:41:15 +0100

    update

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,4 +1,4 @@
-import Homepage from "@/pages/Homepage";
+import Homepage from "../pages/Homepage";
 import { createBrowserRouter } from "react-router-dom";
 
 export const routes=createBrowserRouter([{
