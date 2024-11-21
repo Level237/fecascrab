790f02931586fcc5274f13d354b5653bfd93e9e1
level237 <Bramslevel129@gmail.com>
2024-11-21 14:36:15 +0100

    timeline in action

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,7 +1,36 @@
 import { Button } from '../components/ui/button'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Homepage() {
+
+  const [startDate,setStartDate]=useState(Date.now())
+  const [endDate,setEndDate]=useState('2025-03-12T10:00')
+  const [timeRemaining, setTimeRemaining] = useState({});
+  const calculerTempsRestant = (d1:any, d2:any) => {
+    const now:any = new Date();
+    const d2Date:any = new Date(d2);
+    const diff = d2Date - now;
+
+    if (diff < 0) {
+        return { jours: 0, heures: 0, minutes: 0, secondes: 0 };
+    }
+
+    const jours = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const heures = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const secondes = Math.floor((diff % (1000 * 60)) / 1000);
+
+    return { jours, heures, minutes, secondes };
+};
+
+useEffect(() => {
+  const interval = setInterval(() => {
+      setTimeRemaining(calculerTempsRestant(startDate,endDate));
+  }, 1000);
+
+  return () => clearInterval(interval);
+}, [startDate, endDate]);
+
   return (
     <section style={{ background:"url('BG_Site.jpg')",backgroundPosition:"top",backgroundSize:"cover",backgroundRepeat:"no-repeat" }} 
     className='w-full bg-[#02abee] rounded-md relative h-[100vh] justify-start pt-20 flex flex-col items-center'>
@@ -15,7 +44,7 @@ export default function Homepage() {
         <div className='flex gap-3 justify-center flex-col items-center'>
         <div className='flex gap-1 items-center flex-row'>
                 <div className='bg-white rounded-xl px-3 py-3'>
-                      <h2 className='text-5xl font-bold'>0</h2>
+                      <h2 className='text-5xl font-bold'>{timeRemaining.jours}</h2>
                 </div>
                 <div className='bg-white rounded-xl px-3 py-3'>
                       <h2 className='text-5xl font-bold'>0</h2>
@@ -32,7 +61,7 @@ export default function Homepage() {
         <div className='flex gap-3 justify-center flex-col items-center'>
         <div className='flex gap-1 items-center flex-row'>
                 <div className='bg-white rounded-xl px-3 py-3'>
-                      <h2 className='text-5xl font-bold'>0</h2>
+                      <h2 className='text-5xl font-bold'>{timeRemaining.heures}</h2>
                 </div>
                 <div className='bg-white rounded-xl px-3 py-3'>
                       <h2 className='text-5xl font-bold'>0</h2>
@@ -48,7 +77,7 @@ export default function Homepage() {
         <div className='flex gap-3 justify-center flex-col items-center'>
         <div className='flex gap-1 items-center flex-row'>
                 <div className='bg-white rounded-xl px-3 py-3'>
-                      <h2 className='text-5xl font-bold'>0</h2>
+                      <h2 className='text-5xl font-bold'>{timeRemaining.minutes}</h2>
                 </div>
                 <div className='bg-white rounded-xl px-3 py-3'>
                       <h2 className='text-5xl font-bold'>0</h2>
@@ -64,15 +93,15 @@ export default function Homepage() {
         <div className='flex gap-3 justify-center flex-col items-center'>
         <div className='flex gap-1 items-center flex-row'>
                 <div className='bg-white rounded-xl px-3 py-3'>
-                      <h2 className='text-5xl font-bold'>0</h2>
+                      <h2 className='text-5xl font-bold'>{timeRemaining?.secondes}</h2>
                 </div>
                 <div className='bg-white rounded-xl px-3 py-3'>
-                      <h2 className='text-5xl font-bold'>0</h2>
+                      <h2 id='seconde' className='text-5xl font-bold'>0</h2>
                 </div>
               
         </div>
         <div>
-          <h2 className='text-md font-bold text-white'>SECONDES</h2>
+          <h2  className='text-md font-bold text-white'>SECONDES</h2>
         </div>
         </div>
       </div>
