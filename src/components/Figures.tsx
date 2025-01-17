9d48e967b493bbc112ef5751c8dcf8aa683a25f5
level237 <Bramslevel129@gmail.com>
2025-01-17 17:24:13 +0100

    update

--- src/components/Figures.tsx
+++ src/components/Figures.tsx
@@ -1,42 +1,22 @@
 import medalOr from "../assets/gold medal.png"
 import medalArgent from "../assets/silver medal.png"
 import medamBronze from "../assets/bronze medal.png"
+import AnimatedCount from "./animated-count"
 
 export default function Figures() {
   return (
     <div className='flex flex-row  max-sm:flex-col items-center mt-12 max-sm:gap-12   justify-center'>
       <div className='flex gap-2 mr-24 flex-col max-sm:justify-between max-sm:mr-0  items-center'>
         <img className="w-36 h-36 max-sm:w-28 max-sm:h-28" src={medalOr} alt="" />
-        <div>
-            <h2 className='text-5xl max-sm:text-4xl font-bold text-red-700'>14</h2>
-        </div>
-        <div className=''>
-            <h2 className='text-2xl text-center  font-bold text-black'>médailles
-                <br />
-                d'or</h2>
-        </div>
+        <AnimatedCount count={14} index={1} type="d'or" />
       </div>
       <div className='flex gap-2 mr-24 flex-col max-sm:justify-center max-sm:mr-0  items-center'>
           <img className="w-36 h-36 max-sm:w-28 max-sm:h-28" src={medalArgent} alt="" />
-      <div>
-            <h2 className='text-5xl max-sm:text-4xl font-bold text-red-700'>09</h2>
-        </div>
-        <div className=''>
-            <h2 className='text-2xl text-center  font-bold text-black'>médailles 
-                <br />
-            d'argent</h2>
-        </div>
+          <AnimatedCount count={9} index={2} type="d'argent" />
       </div>
       <div className='flex gap-2 flex-col items-center'>
           <img className="w-36 h-36 max-sm:w-28 max-sm:h-28" src={medamBronze} alt="" />
-      <div>
-            <h2 className='text-5xl max-sm:text-4xl font-bold text-red-700'>09</h2>
-        </div>
-        <div className=''>
-            <h2 className='text-2xl text-center  font-bold text-black'>médailles
-                <br />
-                de bronze</h2>
-        </div>
+          <AnimatedCount count={9} index={3} type="de bronze" />
       </div>
     </div>
   )
---
+++ src/components/animated-count.tsx
@@ -0,0 +1,39 @@
+import { animate, useInView,motion } from 'framer-motion'
+import React, { useEffect, useRef, useState } from 'react'
+
+export default function AnimatedCount({count,index,type}:{count:number,index:number,type:string}) {
+    const ref = useRef(null)
+    const isInView = useInView(ref, { once: true })
+    const [displayCount, setDisplayCount] = useState(0)
+    useEffect(() => {
+        if (isInView) {
+          const controls = animate(0, count, {
+            duration: 2,
+            onUpdate: (value) => setDisplayCount(Math.floor(value)),
+            ease: "easeOut",
+            delay: index * 0.2,
+          })
+    
+          return () => controls.stop()
+        }
+      }, [isInView, count, index])
+  return (
+    <div ref={ref} className="flex flex-col items-center space-y-2">
+        <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={isInView ? { opacity: 1, y: 0 } : {}}
+        transition={{ duration: 0.8, delay: index * 0.2 }}
+        className="text-center"
+      >
+        <div className="text-5xl max-sm:text-4xl font-bold text-red-700">
+          {displayCount.toString().padStart(2, '0')}
+        </div>
+        <div className="text-2xl text-center mt-4  font-bold text-black">
+          médailles
+          <br />
+          {type}
+        </div>
+      </motion.div>
+    </div>
+  )
+}
